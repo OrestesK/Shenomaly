f3e37d7adb72db51dd9600f87818861cf7125881
OrestesK <oresteskassian@gmail.com>
2023-10-27 19:38:50 -0400

    better collision

--- scripts/cowboy_body.gd
+++ scripts/cowboy_body.gd
@@ -1,7 +1,18 @@
 extends CharacterBody2D
 
 @onready var _sprite = $cowboy_sprite
+@export var speed = 100
 
 func _process(_delta):
 	#plays the bop (idle) animation
 	_sprite.play("bop")
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		print("I collided with ", collision.get_collider().name)
