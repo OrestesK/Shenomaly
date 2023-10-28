c159c3ea412606e3153b705692865e27dbf2abd4
OrestesK <oresteskassian@gmail.com>
2023-10-27 23:59:36 -0400

    half did animations

--- scripts/cowboy.gd
+++ scripts/cowboy.gd
@@ -1,10 +1,35 @@
 extends CharacterBody2D
 
 @export var speed = 100
+var sprite: AnimatedSprite2D
 
+var half = PI/2
+
+func _ready():
+	sprite = $CowboySprite
+	
+func set_sprite():
+	if velocity == Vector2(0, 0) : 
+		sprite.play("bop_front")
+		return
+	
+	var angle = velocity.angle()
+	print(angle)
+	
+	if angle == 0:
+		sprite.set_flip_h( false )
+		sprite.play("walk_front")
+	elif angle > half:
+		sprite.set_flip_h( true )
+		sprite.play("walk_front")
+	elif angle < 0:
+		sprite.play("walk_back")
+	else:
+		sprite.play("walk_front")
+		
 func _process(_delta):
-	#plays the bop (idle) animation
-	$CowboySprite.play("bop")
+	set_sprite()
+
 	
 
 func get_input():
