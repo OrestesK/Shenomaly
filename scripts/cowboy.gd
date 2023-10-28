b2e36811226f2362b8c611bc4e1ca50f6c40dac7
Robert White <rjwhitenh@outlook.com>
2023-10-27 21:09:31 -0400

    Added collision to the corral

--- scripts/cowboy.gd
+++ scripts/cowboy.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@onready var _sprite = $cowboy_sprite
 @export var speed = 100
 
 func _process(_delta):
 	#plays the bop (idle) animation
-	_sprite.play("bop")
+	$CowboySprite.play("bop")
+	
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -13,6 +13,4 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		print("I collided with ", collision.get_collider().name)
+	move_and_slide()
